9eac8cc752dd313d42c03ccbe783131fed0a7c37
Miss Ibeh <ibehprincess13@gmail.com>
2025-10-23 12:16:30 -0700

    completed meal-app using tailwind.css and react.js

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
-import { AiOutlineMenu } from "react-icons/ai";
-import { AiOutlineSearch } from "react-icons/ai";
-import { BsFillCartFill } from "react-icons/bs";
+import React, { useState } from 'react'
+import { AiOutlineClose, AiOutlineMenu, AiOutlineSearch, AiFillTag  } from "react-icons/ai";
+import { BsFillCartFill, BsFillSaveFill } from "react-icons/bs";
+import { TbTruckDelivery } from "react-icons/tb";
+import { MdFavorite, MdHelp } from "react-icons/md";
+import { FaWallet, FaUserFriends} from "react-icons/fa";
 
 
 
 const Navbar = () => {
+    const  [ nav, setNav ] = useState(false);
   return (
     <div className='max-w-[1640px] mx-auto p-4 flex justify-between items-center'>
       {/*left-side*/}
       <div className='flex items-center'>
-        <div>
+        <div onClick={() => setNav(!nav)}
+        className='cursor-pointer'>
+            {/** we add react icons: in termiinal type npm install reac-icons
+             * we can also go to react-icons on google. Then type menu in the search bar
+             * to get what we wrote below
+             */}
            <AiOutlineMenu size={30} /> 
         </div>
         <h1 className='text-2xl sm:text-3xl lg:text-4xl px-2'>
@@ -33,6 +41,43 @@ const Navbar = () => {
         <BsFillCartFill size={20}/>
         Cart
      </button>
+
+
+     {/**Mobile Menu */}
+     {/**Overlay */}
+    {/*<div className='bg-black/80 fixed w-full h-screen z-10 left-0 top-0' ></div>*/}
+
+    {/** The hook (useState)is used here */}
+    { nav ? <div className='bg-black/80 fixed w-full h-screen z-10 left-0 top-0'></div>
+    :''}
+
+    {/** Side slide Drawer */}
+   {/* <div className='fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300' ></div>*/}
+
+   {/**The hook (useState) is used here */}
+   <div className={nav ? 'fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300': 'fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-300'}>
+    {/**lets put a close button */}
+    {/**<AiOutlineClose size={30} className='absolute right-4 top-6 cursor-pointer> */}
+
+    {/**We use the hook (useState) as well to close the slide */}
+    <AiOutlineClose 
+    onClick={() => setNav(!nav)}
+    size={30} className='absolute right-4 top-6 cursor-pointer'/>
+    <h2 className='text-2xl p-6'>
+        Best <span className='font-bold'>Eats</span>
+    </h2>
+    <nav>
+        <ul className='flex flex-col p-6 text-gray-800'>
+            <li className='text-xl py-4 flex items-center'><TbTruckDelivery size={25} className='mr-4'/> Orders</li>
+            <li className='text-xl py-4 flex items-center'><MdFavorite size={25} className='mr-4'/>Favourites</li>
+            <li className='text-xl py-4 flex items-center'><FaWallet size={25} className='mr-4'/>Wallet</li>
+            <li className='text-xl py-4 flex items-center'><MdHelp size={25} className='mr-4'/>Help</li>
+            <li className='text-xl py-4 flex items-center'><AiFillTag size={25} className='mr-4'/>Promotions</li>
+            <li className='text-xl py-4 flex items-center'><BsFillSaveFill size={25} className='mr-4'/>Best One</li>
+            <li className='text-xl py-4 flex items-center'><FaUserFriends size={25} className='mr-4'/>Invite Friends</li>
+        </ul>
+    </nav>
+   </div>
     </div>
     </div>
   )
